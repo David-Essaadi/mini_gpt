6efd44b677f43816966e7d0135295c37d143913c
David Essaadi <david@sephia.io>
2024-04-18 19:53:30 +0200

    initial project setup

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pythonPackages = pkgs.python312.withPackages (ps: [ ]);
+    in
+    {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = [ pythonPackages ];
+      };
+    };
+}
